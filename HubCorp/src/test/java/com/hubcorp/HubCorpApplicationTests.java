15c15053dafe166ed21c3c54bdcae1844b150834
Thales Calil <thaleshcalil@gmail.com>
2026-08-17 19:01:41 -0300

    add back-end files with SpringBoot configuration

---
+++ HubCorp/src/test/java/com/hubcorp/HubCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubcorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubCorpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
